349b9628b6d56166ee8f7e1c9f536e121a92f25b
Charlie Beckwith <charlie.beckwith@marquette.edu>
2014-01-29 15:15:44 -0600

    Didn't fix anything.java

--- Homwork2/src/com/beckwith/Game/Game.java
+++ Homwork2/src/com/beckwith/Game/Game.java
@@ -3,6 +3,8 @@ package com.beckwith.Game;
 import java.applet.Applet;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -11,71 +13,85 @@ import com.beckwith.framework.ObjectID;
 import com.beckwith.objects.Garbage;
 import com.beckwith.objects.Player;
 
+public class Game extends Applet implements Runnable, MouseMotionListener {
 
-public class Game extends Applet implements Runnable{
-
-	
 	public static LinkedList<GameObject> objects;
 	public static int WIDTH, HEIGHT;
-	
-	
+	private Player player;
+
 	Thread t = null;
-	Image imageBuffer =null;
+	Image imageBuffer = null;
 	Graphics buffer = null;
 	Random rand = new Random();
-	
-	
-	public void init(){
+
+	public void init() {
 		WIDTH = this.getWidth();
 		HEIGHT = this.getHeight();
 		objects = new LinkedList<GameObject>();
+		addMouseMotionListener(this);
 	}
-	
+
 	public void run() {
-		while (t != null)
-	    {
-		    repaint();
-		    try {
-		    	Thread.sleep(40);
-		    } catch (InterruptedException e) {}
-	    }
+		while (t != null) {
+			repaint();
+			try {
+				Thread.sleep(40);
+			} catch (InterruptedException e) {
+			}
+		}
 	}
-	
-	public void start(){
-		Player player = new Player(WIDTH/2,HEIGHT/2,0, ObjectID.player);
+
+	public void start() {
+		player = new Player(WIDTH / 2, HEIGHT / 2, 0, ObjectID.player);
 		Garbage garbage = new Garbage(20, 35, 0, ObjectID.garbage);
-		
+
 		objects.add(player);
 		objects.add(garbage);
-		//Start Thread
+
+		// Start Thread
 		t = new Thread(this);
 		t.start();
-		
+
 	}
-	
-	public void update(Graphics g){
-		for(GameObject go : objects){
+
+	public void update(Graphics g) {
+		for (GameObject go : objects) {
 			go.tick();
 		}
-		
-	  	if (imageBuffer == null)
-	  	{
-	  		imageBuffer = createImage(WIDTH,HEIGHT);
-	  		buffer = imageBuffer.getGraphics();
-	  	}
-	  	
-	  	paint(buffer);
-	  	g.drawImage(imageBuffer,0,0,null);
+
+		if (imageBuffer == null) {
+			imageBuffer = createImage(WIDTH, HEIGHT);
+			buffer = imageBuffer.getGraphics();
+		}
+
+		paint(buffer);
+		g.drawImage(imageBuffer, 0, 0, null);
 	}
-	
-	public void paint(Graphics g) {		
-		for(GameObject go : objects){
+
+	public void paint(Graphics g) {
+		for (GameObject go : objects) {
 			go.render(g);
 		}
 	}
-	
+
 	public void stop() {
-	    t = null;  // stop animation thread
-	  } 
-	
+		t = null; // stop animation thread
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+
+		if (player != null) {
+			player.setDegrees(player.calculateDegrees(WIDTH/2, HEIGHT/2, e.getX(), e.getY()));
+			
+		}
+
+	}
+
 }
--- Homwork2/src/com/beckwith/objects/Player.java
+++ Homwork2/src/com/beckwith/objects/Player.java
@@ -7,6 +7,9 @@ import java.awt.PaintContext;
 import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
@@ -19,30 +22,36 @@ public class Player extends GameObject {
 
 	private Color playerColor = Color.GRAY;
 	private Polygon triangle;
-	private Double rotateDegrees;
+	private Double rotateDegrees = 0.0;
 
 	public Player(int x, int y, int speed, ObjectID objID) {
 		super(x, y, speed, objID);
-		
-		int xPoints[] = { 0, 20, 0 };
-		int yPoints[] = { 0, 20, 20 };
-		triangle = new Polygon(xPoints, yPoints, 3);
-		rotateDegrees = 0.0;
 
+		// int xPoints[] = { 0, 20, 0 };
+		// int yPoints[] = { 0, 20, 20 };
+
+		int xPoints[] = { 10, 10, 20, 0, 15, 30, 20, 20, 10 };
+		int yPoints[] = { 0, 20, 20, 20, 30, 20, 20, 0, 0 };
+
+		triangle = new Polygon(xPoints, yPoints, 9);
+		
+		
 	}
 
 	@Override
 	public void tick() {
-		rotateDegrees++;
 
 	}
 
 	@Override
 	public void render(Graphics g) {
 		// TODO Auto-generated method stub
-		Graphics2D g2d = (Graphics2D) g;
+		Graphics2D g2d = (Graphics2D) g.create();
 		BufferedImage img = createRotatedObject();
+		
 		g2d.drawImage(img, X, Y, null);
+		g2d.dispose();
+		
 	}
 
 	@Override
@@ -50,18 +59,42 @@ public class Player extends GameObject {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	private BufferedImage createRotatedObject(){
-		BufferedImage img = new BufferedImage(40, 40, BufferedImage.TRANSLUCENT);
+
+	private BufferedImage createRotatedObject() {
 		
+		BufferedImage img = new BufferedImage(60, 60, BufferedImage.TRANSLUCENT);
+
 		Graphics2D g = img.createGraphics();
-		g.rotate(Math.toDegrees(rotateDegrees), 20, 20);
-		
+//		g.rotate(Math.toRadians(rotateDegrees), triangle.getBounds()
+//				.getCenterX(), triangle.getBounds().getCenterY());
+		System.out.println("Rotate Degrees = " + rotateDegrees);
+
 		g.setColor(Color.RED);
 		g.fill(triangle);
 		g.dispose();
-		return img;
-		
+		BufferedImage rot  = new BufferedImage(60,60, BufferedImage.TRANSLUCENT);
+		AffineTransform xform = new AffineTransform();
+		xform.translate(triangle.getBounds().getCenterY(), triangle.getBounds().getCenterX());
+		xform.rotate(rotateDegrees);
+		xform.translate(-triangle.getBounds().getCenterY(), -triangle.getBounds().getCenterX());
+		Graphics2D g2d = (Graphics2D) rot.createGraphics();
+		g2d.drawImage(img,xform,null);
+		g2d.dispose();
+		return rot;
+
 	}
 
+
+
+	public double calculateDegrees(int CenterX, int CenterY, int mouseX, int mouseY) {
+		double angle = Math.atan2(CenterY + triangle.getBounds()
+				.getCenterY() - mouseY, CenterX + triangle.getBounds()
+				.getCenterX() - mouseX) - Math.PI
+				/ 2;
+		return angle;
+	}
+	
+	public void setDegrees(double degrees){
+		rotateDegrees = degrees;
+	}
 }
