41b6ef2d8c857a006e172740b29766c7bdfaa4db
Charlie Beckwith <charlie.beckwith@marquette.edu>
2014-01-30 11:55:03 -0600

    Made a lot of changes again

--- Homwork2/src/com/beckwith/Game/Game.java
+++ Homwork2/src/com/beckwith/Game/Game.java
@@ -10,6 +10,8 @@ import java.awt.event.MouseMotionListener;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.beckwith.framework.GameObject;
 import com.beckwith.framework.ObjectID;
@@ -30,6 +32,8 @@ public class Game extends Applet implements Runnable, MouseMotionListener,
 	Graphics buffer = null;
 	Random rand = new Random();
 
+	Timer timer = new Timer();
+
 	public void init() {
 		WIDTH = this.getWidth();
 		HEIGHT = this.getHeight();
@@ -42,7 +46,7 @@ public class Game extends Applet implements Runnable, MouseMotionListener,
 		Background bg = new Background(0, 0, WIDTH, HEIGHT, 0, Color.white,
 				ObjectID.background);
 		player = new Player(WIDTH / 2, HEIGHT / 2, 0, ObjectID.player);
-		
+
 		objects.add(bg);
 		objects.add(player);
 		addGarbage();
@@ -59,25 +63,25 @@ public class Game extends Applet implements Runnable, MouseMotionListener,
 			}
 		}
 	}
-	
-	public void addGarbage(){
-		int boundX = (WIDTH -150) / 2;
+
+	public void addGarbage() {
+		int boundX = (WIDTH - 150) / 2;
 		int boundY = (HEIGHT - 150) / 2;
-		
-		int rand1[] = {rand.nextInt(175), rand.nextInt(175)};
-		int rand2[] = {rand.nextInt(175)+325, rand.nextInt(175) + 325};
-		if(rand.nextBoolean()){
-			if(rand.nextBoolean()){
-				if(rand.nextBoolean()){
+
+		int rand1[] = { rand.nextInt(175), rand.nextInt(175) };
+		int rand2[] = { rand.nextInt(175) + 325, rand.nextInt(175) + 325 };
+		if (rand.nextBoolean()) {
+			if (rand.nextBoolean()) {
+				if (rand.nextBoolean()) {
 					rand1 = rand2;
 				}
-			}else{
+			} else {
 				rand1[0] = rand2[1];
 			}
-		}else{
+		} else {
 			rand1[1] = rand2[0];
 		}
-		
+
 		System.out.println("rand[0]" + rand1[0] + "  rand[1] " + rand1[1]);
 		Garbage garbage = new Garbage(rand1[0], rand1[1], 0, ObjectID.garbage);
 		objects.add(garbage);
@@ -94,11 +98,12 @@ public class Game extends Applet implements Runnable, MouseMotionListener,
 	public void checkCollisions(GameObject object) {
 		for (GameObject go : objects) {
 			if (go.getCollider() != null) {
-				if (go.checkCollision(object.getCollider()) && go.getID() != object.getID()) {
+				if (go.checkCollision(object.getCollider())
+						&& go.getID() != object.getID()) {
 					System.out.println("Collision detected between "
 							+ go.getID() + " & " + object.getID());
-					 go.destroy();
-					 object.destroy();
+					go.destroy();
+					object.destroy();
 				}
 			}
 		}
@@ -116,6 +121,7 @@ public class Game extends Applet implements Runnable, MouseMotionListener,
 			if (!object.isAlive()) {
 				System.out.println("Object destroyed  : " + object.getID());
 				go.remove();
+				
 			}
 			if (object.getID() == ObjectID.bullet) {
 				checkCollisions(object);
@@ -182,13 +188,27 @@ public class Game extends Applet implements Runnable, MouseMotionListener,
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-
+		if (e.isControlDown()) {
+			task = new MyTimerTask();
+			timer.scheduleAtFixedRate(task, 0, 10);
+		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
+		task.cancel();
 
+	}
+
+	TimerTask task = new MyTimerTask();
+
+	private class MyTimerTask extends TimerTask {
+		public void run() {
+			double[] pos = player.getTurretPosition();
+			Bullet bullet = new Bullet((int) pos[0], (int) pos[1], 5,
+					player.getDegrees(), ObjectID.bullet);
+			objects.add(bullet);
+		}
 	}
 
 }
